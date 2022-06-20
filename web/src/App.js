6f6b81887f1c797e8c06d3714e8172379fd97b1d
Vikas-jangid <vikas.jangid@infobeans.com>
2022-06-20 12:16:30 +0530

    internationalization setup done

--- web/src/App.js
+++ web/src/App.js
@@ -10,22 +10,27 @@ import EditUser from './Components/editUser';
 import ResetPassword from './Components/resetPassword';
 import ForgetPassword from './Components/forgetPassword';
 import Home from './Components/home'
+import i18n from './Components/i18n';
+import { Suspense } from 'react';
+import Loading from './Components/loading.js'
 
 function App() {
 
   return (
     <Router>
-      <Appbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/forgetPassword" element={<ForgetPassword />} />
-        <Route path="/resetPassword/:token" element={<ResetPassword />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/edit/:id" element={<EditUser />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
+      <Suspense fallback={<Loading />}>
+        <Appbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/forgetPassword" element={<ForgetPassword />} />
+          <Route path="/resetPassword/:token" element={<ResetPassword />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/edit/:id" element={<EditUser />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
